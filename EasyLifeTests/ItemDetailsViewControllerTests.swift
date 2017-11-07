7981d9c672294df0e92d3478362a38934d2ad761
Lee Arromba <larromba@gmail.com>
2017-11-07 12:00:33 +0100

    preparing 1.3.1
    * Fixed bug when selecting 'later' for an event far in the past
    * Improved ordering for 'missed' and 'today' sections

--- EasyLifeTests/ItemDetailsViewControllerTests.swift
+++ EasyLifeTests/ItemDetailsViewControllerTests.swift
@@ -13,12 +13,10 @@ import CoreData
 class ItemDetailsViewControllerTests: XCTestCase {
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
         UIView.setAnimationsEnabled(false)
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
         UIView.setAnimationsEnabled(true)
     }
